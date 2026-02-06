d9c55ed51095eb6e06ea3959cab81201db12365b
kevin <kin.siu1@ucdconnect.ie>
2026-02-06 12:09:07 +0000

    push function

--- src/project20280/stacksqueues/ArrayStack.java
+++ src/project20280/stacksqueues/ArrayStack.java
@@ -17,7 +17,7 @@ public class ArrayStack<E> implements Stack<E> {
     /**
      * Index of the top element of the stack in the array.
      */
-    private final int top = -1;                      // index of the top element in stack
+    private int top = -1;                      // index of the top element in stack
 
     /**
      * Constructs an empty stack using the default array capacity.
@@ -35,7 +35,6 @@ public class ArrayStack<E> implements Stack<E> {
     public ArrayStack(int capacity) {        // constructs stack with given capacity
         // TODO
        data = (E[]) new Object[capacity];
-
     }
 
     /**
@@ -67,7 +66,8 @@ public class ArrayStack<E> implements Stack<E> {
     @Override
     public void push(E e) {
         // TODO
-
+        top++;
+        data[top] = e;
     }
 
     /**
@@ -115,23 +115,8 @@ public class ArrayStack<E> implements Stack<E> {
      */
     public static void main(String[] args) {
         Stack<Integer> S = new ArrayStack<>();  // contents: ()
-        S.push(5);                              // contents: (5)
-        S.push(3);                              // contents: (5, 3)
-        System.out.println(S.size());           // contents: (5, 3)     outputs 2
-        System.out.println(S.pop());            // contents: (5)        outputs 3
-        System.out.println(S.isEmpty());        // contents: (5)        outputs false
-        System.out.println(S.pop());            // contents: ()         outputs 5
-        System.out.println(S.isEmpty());        // contents: ()         outputs true
-        System.out.println(S.pop());            // contents: ()         outputs null
-        S.push(7);                              // contents: (7)
-        S.push(9);                              // contents: (7, 9)
-        System.out.println(S.top());            // contents: (7, 9)     outputs 9
-        S.push(4);                              // contents: (7, 9, 4)
-        System.out.println(S.size());           // contents: (7, 9, 4)  outputs 3
-        System.out.println(S.pop());            // contents: (7, 9)     outputs 4
-        S.push(6);                              // contents: (7, 9, 6)
-        S.push(8);                              // contents: (7, 9, 6, 8)
-        System.out.println(S.pop());            // contents: (7, 9, 6)  outputs 8
+
+        S.push(100);
         System.out.println(S);
     }
 }
